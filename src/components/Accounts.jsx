33fa385a8d6d3026b60df78645ae346a5993e2c3
AlexKvazos <contact@alexkvazos.com>
2015-10-09 09:21:13 -0500

    Empty states

--- src/components/Accounts.jsx
+++ src/components/Accounts.jsx
@@ -5,7 +5,22 @@ class Accounts extends React.Component {
   render() {
     return (
       <div>
-        <Header title='Accounts' />
+        <Header title='Accounts' button={(
+          <div className='icon right'>
+            <i className={ `fa fa-plus` }></i>
+          </div>
+        )} />
+
+        <div className='container'>
+          <div className='empty no-select'>
+            <p><i className='fa fa-users'></i></p>
+            <p>You haven't added any accounts!</p>
+            <p className='instructions'>
+              1. Tap the '+' button on the top right<br />
+              2. Input your account information and tap 'Save'.
+            </p>
+          </div>
+        </div>
 
       </div>
     );
--- src/components/Servers.jsx
+++ src/components/Servers.jsx
@@ -6,7 +6,25 @@ class Servers extends React.Component {
   render() {
     return (
       <div>
-        <Header title='Servers' />
+        <Header title='Servers' button={(
+          <div className='icon right'>
+            <i className={ `fa fa-plus` }></i>
+          </div>
+        )} />
+
+        <div className='container'>
+          <div className='empty no-select'>
+            <p><i className='fa fa-server'></i></p>
+            <p>You haven't added any servers!</p>
+            <p className='instructions'>
+              1. Tap the '+' button on the top right<br />
+              2. Input the server information and tap 'Save'.
+            </p>
+            <p className='instructions note'>
+              Note: MinecraftChat only works on 1.8 servers.
+            </p>
+          </div>
+        </div>
 
       </div>
     );
