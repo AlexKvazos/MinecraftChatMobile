837136f2c487cfc19786e9de908aeb687534fc8e
AlexKvazos <contact@alexkvazos.com>
2015-10-10 16:26:54 -0500

    Hide accessory bar

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@ function init() {
 
   // prevent keyboard scrolling our view
   if (window.cordova && window.cordova.plugins.Keyboard) {
+    window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     window.cordova.plugins.Keyboard.disableScroll(true);
   }
 
